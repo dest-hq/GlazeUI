52d90f55e6fe76ec455261ddf1c89504635f3bad
ARandomOSDever <104090029+ARandomOSDever@users.noreply.github.com>
2026-01-21 08:55:46 +0200

    add more comments to hello world code

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -13,7 +13,7 @@ fn main() -> Result<(), Error> {
         text: "Hello world!".to_string(),
     };
 
-    start(app).title("Hello world!").size(900, 900).vsync(false).run()
+    start(app).title("Hello world!").size(900, 900).run()
     // This starts the app with the title of "Hello world!" and with the width and height of 900 pixels
     // P.S. You can add ".vsync(false)" between "start(app)" and ".run()" to disable VSync which will make your GPU draw the app as fast as it can
     // Note: As of writing this, disabling VSync will flood your terminal with the amount of frames per second every frame and will also induce a high load on your GPU
@@ -38,13 +38,12 @@ impl Application for HelloWorld {
 
     fn view(&self) -> Widget<Self::Message> {
         let hello_world_text = text(&self.text)
-            .size(36)
-            .weight(text::TextWeight::NORMAL)
-            .into();
+            .size(36) // Set text size to 36 pixels
+            .into(); // Turn text element into Widget
         
         vstack!(hello_world_text) // VStack is used to vertically stack multiple elements vertically, but since we have only 1 element a HStack could also be used here
         .horizontal_align(glazeui::widgets::utils::types::HorizontalAlign::Center)
         .vertical_align(glazeui::widgets::utils::types::VerticalAlign::Center)
-        .into()
+        .into() // Turn VStack to Widget
     }
 }
