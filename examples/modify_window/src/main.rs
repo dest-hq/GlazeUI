208d5ec98c9d074a1e43be3963130d5a80fa310e
mxghj <dest-supp@proton.me>
2026-01-23 16:13:12 +0200

    Add new example
    Added a modify_window example

---
+++ examples/modify_window/src/main.rs
@@ -0,0 +1,55 @@
+use glazeui::{Error, Window, core::ui::Ui, start};
+
+fn main() -> Result<(), Error> {
+    let app = ModifyWindow {};
+    start(app, ModifyWindow::view).run()
+}
+
+struct ModifyWindow {}
+
+impl ModifyWindow {
+    fn view(&mut self, ui: &mut Ui<ModifyWindow>) {
+        let close = ui
+            .button("Close")
+            .label_size(25)
+            .size(300.0, 50.0)
+            .on_click(|_app: &mut ModifyWindow, window: &mut Window| window.close())
+            .build();
+        let change_title = ui
+            .button("Change title to Hi")
+            .label_size(25)
+            .size(300.0, 50.0)
+            .on_click(|_app: &mut ModifyWindow, window: &mut Window| window.title("Hi"))
+            .build();
+        let off_decorations = ui
+            .button("Off decorations")
+            .label_size(25)
+            .size(300.0, 50.0)
+            .on_click(|_app: &mut ModifyWindow, window: &mut Window| window.decorations(false))
+            .build();
+        let on_decorations = ui
+            .button("On decorations")
+            .label_size(25)
+            .size(300.0, 50.0)
+            .on_click(|_app: &mut ModifyWindow, window: &mut Window| window.decorations(true))
+            .build();
+        let off_resizable = ui
+            .button("Off resizable")
+            .label_size(25)
+            .size(300.0, 50.0)
+            .on_click(|_app: &mut ModifyWindow, window: &mut Window| window.resizable(false))
+            .build();
+
+        ui.vstack(vec![
+            close,
+            change_title,
+            off_decorations,
+            on_decorations,
+            off_resizable,
+        ])
+        .align(glazeui::types::Align::Center)
+        .spacing(20.0)
+        .length(glazeui::types::Length::Fill)
+        .show();
+    }
+}
